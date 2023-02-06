c076e7fde34256cefee0acf8288ef60bfa4c7bd3
3030747 <3030747@30307-47>
2023-02-06 14:21:54 +0900

    asd

--- src/Spring/aaa.java
+++ src/Spring/aaa.java
@@ -1,12 +1,3 @@
 
 public class aaa {
-<<<<<<< HEAD
-	aavvv as aaaazzzas afd
-=======
-<<<<<<< HEAD
-	aavvv hg
-=======
-	aavvv as
->>>>>>> branch 'main' of https://github.com/wakisaka302/Spring
->>>>>>> branch 'main' of https://github.com/wakisaka302/Spring
 }
