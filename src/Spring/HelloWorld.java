4102c2b65c5096ce4523fe41f09c64d6140f5769
wakisaka302 <natirouga32@gmail.com>
2023-02-07 14:21:16 +0900

    asd

--- src/Spring/HelloWorld.java
+++ src/Spring/HelloWorld.java
@@ -3,13 +3,8 @@ package lesson_0_0_1;
 public class HelloWorld {
 	public HelloWorld() {
 		System.out.println("Hello Worsld!");
-<<<<<<< HEAD
 		
 		aa vvyy efse
-=======
-		f
-		aa vvyyfffff hj ad tahara
->>>>>>> branch 'main' of https://github.com/wakisaka302/Spring
 		
 	}
 }
